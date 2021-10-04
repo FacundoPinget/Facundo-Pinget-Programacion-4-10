194aea2419162546487c0ad043ed765f245cc8dd
FacundoPinget <84210988+FacundoPinget@users.noreply.github.com>
2021-10-04 20:30:43 -0300

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,22 @@
+#EJ1
+
+variable1 = " tragaban trigo en un trigal"
+variable2 = " tragaban trigo tres tristes tigres"
+variable3 = "tres tristes tigres"
+variable4 = " en tres tristes trastos"
+
+variable5 = print(variable3 + variable1 + variable4+ variable2)
+print(variable5)
+
+#Ej2
+
+cadena_invertida = "airotsih,6.7,ttorrag odrareG"
+print(cadena_invertida [::-1])
+
+#EJ3
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = int(input("Ingrese su edad: "))
+numerosuerte = int(input("Ingrese el numero de la suerte "))
+cadena_final = print(nombre+apellido,"Tu numero de la suerte es", edad*numerosuerte)
